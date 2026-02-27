3062aeff81dc4e29b72952122496ac87411d9808
Devanshuk29 <9kumardevanshu@gmail.com>
2026-02-27 13:55:20 +0530

    .env config

--- goal_planner/goal-planner-backend/config/database.js
+++ goal_planner/goal-planner-backend/config/database.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  password: 'Devansh@29_',
-  host: 'localhost',
-  port: 5432,
-  database: 'goal_planner'
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME
 });
 
 module.exports = pool;
