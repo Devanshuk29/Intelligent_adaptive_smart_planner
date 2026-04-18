742b30a3f3c16746f4a28c1e0ba37d889d1bd31a
Devanshuk29 <9kumardevanshu@gmail.com>
2026-04-19 02:02:04 +0530

    database

--- goal_planner/goal-planner-backend/config/database.js
+++ goal_planner/goal-planner-backend/config/database.js
@@ -6,7 +6,10 @@ const pool = new Pool({
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
-  database: process.env.DATABASE_NAME
+  database: process.env.DATABASE_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
